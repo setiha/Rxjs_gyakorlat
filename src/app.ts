725ab0cb668a15b3c3b1b3e3342562a2c0cfdb5b
eggp <gabor.harangozo@eggproject.hu>
2017-11-22 18:43:14 +0100

    firstobservable + customobservable

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,55 @@
 import { replaceConsoleLog } from '../browser/console.log';
+import 'rxjs/add/observable/of';
 
 replaceConsoleLog();
 
-console.log('test');
+// const firstObservable = Observable.of('value');
+// firstObservable.subscribe(
+//     streamValue => console.log(streamValue)
+// );
+// firstObservable.subscribe(
+//     streamValue => console.log(streamValue)
+// );
+
+// const customObservable = new Observable(
+//     observer => {
+//         let tick = 0;
+//         const intervalRef = setInterval(
+//             () => {
+//                 if (tick == 1) {
+//                     observer.complete();
+//                 } else {
+//                     observer.next(tick++);
+//                 }
+//             },
+//             1000
+//         );
+//
+//         return () => {
+//             clearInterval(intervalRef);
+//             console.log('end stream');
+//         };
+//     }
+// );
+// const customObservableSubscription = customObservable.subscribe(
+//     streamValue => {
+//         console.log(`customObservable stream value: ${streamValue}`);
+//     }
+// );
+// setTimeout(() => {
+//     const customObservableSubscription = customObservable.subscribe(
+//         streamValue => {
+//             console.log(`customObservable stream value2: ${streamValue}`);
+//         }
+//     );
+//
+//     setTimeout(() => {
+//         customObservableSubscription.unsubscribe();
+//     }, 3000);
+//
+// }, 5000);
+
+setTimeout(() => {
+    customObservableSubscription.unsubscribe();
+}, 3000);
+
